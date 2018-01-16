e23830eff1652a4b5b9084f50bf2b010f3922047
Xu Zhipei <xuzhipei@gmail.com>
2018-01-16 22:13:01 +0800

    feat: add examples

---
+++ examples/server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Promise = require('bluebird');
+const _ = require('lodash');
+const blackfyre = require('../');
+
+const producer = new blackfyre.Producer({
+  backendType: blackfyre.BackendType.MongoDB,
+  backendOptions: {
+    url: 'mongodb://localhost',
+    collectionName: 'tasks',
+    dbName: 'blackfyre',
+    resultsExpireIn: 600,
+  },
+  brokerType: blackfyre.BrokerType.AMQP,
+  brokerOptions: {
+    url: 'amqp://guest:guest@localhost',
+    exchangeName: 'worker-exchange',
+    queueSuffix: 'queue',
+  },
+  globalRetryOptions: {
+    delayMs: 100,
+    initDelayMs: 100,
+    maxRetry: 3,
+  },
+  isTestMode: false,
+});
+
+setInterval(async() => {
+  console.log(`---- time: ${new Date()} ---`);
+  console.log(JSON.stringify(await producer.checkHealth()));
+}, 1000);
+
+Promise.map(_.range(1000), async() => {
+    return producer
+      .createTask({
+        name: 'example',
+        body: {
+          delay: _.random(100, 300),
+          retry: _.random() > 0.98,
+        },
+      });
+  }, {
+    concurrency: 200,
+  })
+  .catch(console.log)
+  .finally(() => process.exit());
---
+++ examples/worker.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const Promise = require('bluebird');
+const promClient = require('prom-client');
+
+const summary = new promClient.Summary({
+  name: 'job_summary',
+  help: 'Summary of tasks',
+  percentiles: [0.5, 0.75, 0.9, 0.99, 0.999],
+  labelNames: ['state', 'taskName'],
+});
+
+const blackfyre = require('../');
+
+const consumer = new blackfyre.Consumer({
+  backendType: blackfyre.BackendType.MongoDB,
+  backendOptions: {
+    url: 'mongodb://localhost',
+    collectionName: 'tasks',
+    dbName: 'blackfyre',
+    resultsExpireIn: 600,
+  },
+  brokerType: blackfyre.BrokerType.AMQP,
+  brokerOptions: {
+    url: 'amqp://guest:guest@localhost',
+    exchangeName: 'worker-exchange',
+    queueSuffix: 'queue',
+  },
+  preProcess(task) {
+    this.endTimer = summary.startTimer({
+      taskName: task.name,
+    });
+  },
+  postProcess(task, state, errorOrResult) {
+    this.endTimer({
+      state,
+    });
+  },
+});
+
+setInterval(async() => {
+  console.log(`---- time: ${new Date()} ---`);
+  console.log(JSON.stringify(await consumer.checkHealth()));
+  console.log(promClient.register.getSingleMetricAsString('job_summary'));
+}, 5000);
+
+consumer.registerTask({
+  name: 'example',
+  concurrency: 200,
+}, async(data) => {
+  if (data.retry) {
+    await Promise.delay(100);
+    throw new Error('example');
+  }
+  return Promise.delay(data.delay || 500);
+});
